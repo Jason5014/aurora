c0731f7f6af08bd00c2c7f54b546c7b75222c061
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-07-05 09:21:05 +0000

    Deploy Jason5014/aurora to github.com/Jason5014/aurora.git:gh-pages

---
+++ precache-manifest.78db52749c6dc53540bc4ff4a7fb478d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9117404f46a4f2ac64ce915608d1b58d",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.8c117009a9b6d59f65c3.bundle.js"
+  },
+  {
+    "url": "runtime~main.8c117009a9b6d59f65c3.bundle.js"
+  },
+  {
+    "revision": "79b0668a2bd8f3a94bd3",
+    "url": "static/css/main.74ea6557.chunk.css"
+  },
+  {
+    "revision": "5ae4a044b5ce00510cf83ca02f3512bd",
+    "url": "static/media/style.5ae4a044.css"
+  },
+  {
+    "url": "vendors~main.8c117009a9b6d59f65c3.bundle.js"
+  },
+  {
+    "url": "vendors~main.8c117009a9b6d59f65c3.bundle.js.LICENSE.txt"
+  }
+]);
